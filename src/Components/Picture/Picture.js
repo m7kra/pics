13dc05377d0b7a554d26a86f8bf9ccc688bc9d1f
M7kra <luiswbarbosa@gmail.com>
2023-11-19 18:07:29 +0000

    Transformed == into ===

--- src/Components/Picture/Picture.js
+++ src/Components/Picture/Picture.js
@@ -4,7 +4,7 @@ export default function Picture({src, setFullscreen, orientation}) {
 
     const backgroundImage = 'url("'+src+'")';
     return (
-        <div className={'picture ' + orientation + (orientation == 'landscape'? ' col-md-6 col-sm-10 col-12' : ' col-md-4 col-sm-6 col-10')}>
+        <div className={'picture ' + orientation + (orientation === 'landscape'? ' col-md-6 col-sm-10 col-12' : ' col-md-4 col-sm-6 col-10')}>
             <div
                 style={{
                     '--bg-image': backgroundImage
